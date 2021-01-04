abc196772dce8db260f57d84bcb30d855c3dad06
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2021-01-04 14:21:36 -0500

    WIP interfaces.

--- core/frontend/src/render/effects/ParticleEffectBuilder.ts
+++ core/frontend/src/render/effects/ParticleEffectBuilder.ts
@@ -6,42 +6,109 @@
  * @module Rendering
  */
 
-import { Viewport } from "../../Viewport";
 import { UniformContext, UniformArrayParams, UniformParams } from "./Uniform";
 import { VaryingType } from "./VaryingType";
 
+/** The GLSL data types that can be used for properties of particles in a particle effect.
+ * @see [[ParticleEffectBuilder.addProperty]] to define particle properties.
+ * @beta
+ */
+export type ParticlePropertyType = "float" | "vec2" | "vec3" | "vec4";
+
+/** Snippets of GLSL source code used to implement a particle effect.
+ * @see [[ParticleEffectBuilderParams]].
+ * @see [[RenderSystem.createParticleEffectBuilder]] to define a new particle effect.
+ * @beta
+ */
 export interface ParticleEffectSource {
-  update: string;
+  particle: {
+    initialize: string;
+    update: string;
+  },
   render: {
     vertex: string;
     fragment: string;
   }
 }
 
+/** Parameters used to create a [[ParticleEffectBuilder]].
+ * @beta
+ */
 export interface ParticleEffectBuilderParams {
   name: string;
   source: ParticleEffectSource;
 }
 
-export interface ParticleEffectContext<T> {
-  viewport: Viewport;
-  properties: T;
-}
-
+/** Context supplied when computing the values of uniform variables just before updating and rendering a particle system.
+ * The type `T` is typically an interface that holds or can compute the value of each uniform. An instance of `T` typically
+ * correlates with an instance of the particle effect; many such instances can exist with the same set of uniforms but different
+ * values for each.
+ * @see [[ParticleEffectBuilder.addUniform]] and [[ParticleEffectBuilder.addUniformArray]] to define a particle effect's uniforms.
+ * @beta
+ */
 export interface ParticleUniformContext<T> extends UniformContext {
-  properties: T;
+  instance: T;
 }
 
+/** Describes a property of the particles within a particle system.
+ * @see [[ParticleEffectBuilder.addProperty]] to define a particle effect's properties.
+ * @beta
+ */
+export interface ParticlePropertyParams {
+  /** The base name of the property. Must be unique among all properties of the system.
+   * @see [[ParticleEffectBuilder.addProperty]] for a description of how  the base name is used in the shader program.
+   */
+  name: string;
+  /** The GLSL type of the corresponding attribute and varying variable. */
+  type: ParticlePropertyType;
+  /** By default, properties are only used by the effect's "update" shader program. If `forRender` is true, the property will also be available within the "render" shader program. */
+  forRender?: boolean;
+}
+
+/** A particle effect uses two shader programs:
+ *  - An "update" program that updates the properties of each particle and initializes the properties of newborn particles; and
+ *  - A "render" program that draws the particles.
+ * Most of the update program's code is supplied by the author of the particle effect, while most of the render program's code is usually supplied by the [[RenderSystem]].
+ * The two programs may need entirely different sets of uniforms, though some uniforms may be relevant to both programs.
+ * As part of a [[ParticleUniformParams]] or [[ParticleUniformArrayParams]], the scope tells a [[ParticleEffectBuilder]] which program will use the uniform.
+ * @see [[ParticleEffectBuilder.addUniform]] and [[ParticleEffectBuilder.addUniformArray]] to define a particle effect's uniform variables.
+ * @beta
+ */
 export type ParticleUniformScope = "update" | "render" | "both";
+
+/** Parameters used to define a uniform variable for a particle effect.
+ * @see [[ParticleEffectBuilder.addUniform]] to define a uniform variable.
+ * @beta
+ */
 export type ParticleUniformParams<T> = UniformParams<ParticleUniformContext<T>> & { scope: ParticleUniformScope };
+
+/** Parameters used to define an array of uniform variables for a particle effect.
+ * @see [[ParticleEffectBuilder.addUniformArray]] to define a uniform array.
+ * @beta
+ */
 export type ParticleUniformArrayParams<T> = UniformArrayParams<ParticleUniformContext<T>> & { scope: ParticleUniformScope };
 
+/** ###TODO
+ * @beta
+ */
 export interface ParticleEffectBuilder<T> {
   addUniform: (params: ParticleUniformParams<T>) => void;
 
   addUniformArray: (params: ParticleUniformArrayParams<T>) => void;
 
+  /** Adds a varying variable of the specified type to the effect's render shader program. */
   addVarying: (name: string, type: VaryingType) => void;
+
+  /** Defines a property of the particle's within the particle system. The properties of all particles are updated each frame by the effect's "update"
+   * shader program before the particles are rendered. Each property produces an input attribute prefixed with "a_" and an output varying prefixed with "v_".
+   * For example, if `params.name` is "velocity" and `params.type` is "vec3", then the vertex shader will contain:
+   *  ```
+   *  in vec3 a_velocity;
+   *  out vec3 v_velocity;
+   * ```
+   * By default, particle properties are only included in the update shader program; @see [[ParticlePropertyParams.forRender]] to make a property available to the "render" program as well.
+   */
+  addProperty: (params: ParticlePropertyParams) => void;
 
   finish: () => void;
 }
--- core/frontend/src/render/webgl/ParticleEffect.ts
+++ core/frontend/src/render/webgl/ParticleEffect.ts
@@ -6,8 +6,42 @@
  * @module WebGL
  */
 
-import { ParticleEffectBuilder, ParticleEffectBuilderParams } from "../effects/ParticleEffectBuilder";
+import { VaryingType } from "../effects/VaryingType";
+import {
+  ParticleEffectBuilder, ParticleEffectBuilderParams, ParticlePropertyParams, ParticleUniformArrayParams, ParticleUniformParams,
+} from "../effects/ParticleEffectBuilder";
+import { getUniformVariableType, getVaryingVariableType } from "./EffectBuilder";
 
-export function createParticleEffectBuilder<T>(_params: ParticleEffectBuilderParams): ParticleEffectBuilder<T> | undefined {
-  return undefined;
+import { System } from "./System";
+
+class Builder<T> implements ParticleEffectBuilder<T> {
+  private readonly _name: string;
+  // private readonly _updater: ProgramBuilder;
+  // private readonly _renderer: ProgramBuilder;
+
+  public constructor(params: ParticleEffectBuilderParams) {
+    this._name = params.name;
+  }
+
+  public addUniform(_params: ParticleUniformParams<T>): void {
+    // this._updater = createParticleEffectUpdateBuilder(params);
+    // this._renderer = createParticleEffectRenderBuilder(params);
+  }
+
+  public addUniformArray(_params: ParticleUniformArrayParams<T>): void {
+  }
+
+  public addVarying(_name: string, _type: VaryingType): void {
+  }
+
+  public addProperty(_params: ParticlePropertyParams): void {
+  }
+
+  public finish(): void {
+  }
+}
+
+/** @internal */
+export function createParticleEffectBuilder<T>(params: ParticleEffectBuilderParams): ParticleEffectBuilder<T> | undefined {
+  return System.instance.isWebGL2 ? new Builder<T>(params) : undefined;
 }
